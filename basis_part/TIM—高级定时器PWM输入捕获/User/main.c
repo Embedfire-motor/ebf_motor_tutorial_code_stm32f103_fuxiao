60bd8310fe829368175abd701e81b0f81b98c312
LONG <1558193230@qq.com>
2020-09-03 11:54:49 +0800

    增加与电机不相干代码（复制指南者）

---
+++ basis_part/TIM—高级定时器PWM输入捕获/User/main.c
@@ -0,0 +1,104 @@
+/**
+  ******************************************************************************
+  * @file    main.c
+  * @author  fire
+  * @version V1.0
+  * @date    2020-xx-xx
+  * @brief   TIM—高级定时器互补输出
+  ******************************************************************************
+  * @attention
+  *
+  * 实验平台:野火 F103-指南者 STM32 开发板 
+  * 论坛    :http://www.firebbs.cn
+  * 淘宝    :https://fire-stm32.taobao.com
+  *
+  ******************************************************************************
+  */ 
+
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include <stdlib.h>
+#include "./tim/bsp_advance_tim.h"
+#include "./usart/bsp_debug_usart.h"
+
+extern __IO uint16_t IC2Value;
+extern __IO uint16_t IC1Value;
+extern __IO float DutyCycle;
+extern __IO float Frequency;
+/**
+  * @brief  主函数
+  * @param  无
+  * @retval 无
+  */
+int main(void) 
+{
+  
+  /* 初始化系统时钟为72MHz */
+  SystemClock_Config();
+  HAL_Init();
+  /* 初始化串口 */
+  DEBUG_USART_Config();
+    /* 初始化基本定时器定时，1s产生一次中断 */
+  TIMx_Configuration();
+  printf("高级定时器PWM输入捕获实验\n");
+  
+  while(1)
+  {   
+    HAL_Delay(500);
+    printf("IC1Value = %d  IC2Value = %d ", IC1Value, IC2Value);
+    printf("占空比：%0.2f%%   频率：%0.2fHz\n", DutyCycle, Frequency);
+  }
+}
+
+
+/**
+  * @brief  System Clock Configuration
+  *         The system Clock is configured as follow : 
+  *            System Clock source            = PLL (HSE)
+  *            SYSCLK(Hz)                     = 72000000
+  *            HCLK(Hz)                       = 72000000
+  *            AHB Prescaler                  = 1
+  *            APB1 Prescaler                 = 2
+  *            APB2 Prescaler                 = 1
+  *            HSE Frequency(Hz)              = 8000000
+  *            HSE PREDIV1                    = 2
+  *            PLLMUL                         = 9
+  *            Flash Latency(WS)              = 0
+  * @param  None
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.HSEPredivValue = RCC_HSE_PREDIV_DIV1;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL9;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    
+  }
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
+  {
+    
+  }
+}
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
